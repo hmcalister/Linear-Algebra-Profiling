dfd8b34032f1f13b65b1e38d9f73c2b31f7e171a
Hayden McAlister <haydenmcalister49@gmail.com>
2023-01-29 19:02:45 +1300

    Add static defined matrix rust implementation

---
+++ static_rust/time_script.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+echo "trials,multiplications,threads,time"
+for TRIALS in {10..100..10}
+do
+    for MULTIPLICATIONS in {10..100..10}
+    do
+        for THREADS in 1 2 4 8 16 
+        do
+            TIME_BEFORE=$(date +%s%N)
+            ./static_rust_linalg --trials=$TRIALS --multiplications=$MULTIPLICATIONS --threads=$THREADS
+            TIME_AFTER=$(date +%s%N)
+            echo "$TRIALS,$MULTIPLICATIONS,$THREADS,$((TIME_AFTER-TIME_BEFORE))"
+        done
+    done
+done
+
+for TRIALS in {100..1000..100}
+do
+    for MULTIPLICATIONS in {100..1000..100}
+    do
+        for THREADS in 1 2 4 8 16
+        do
+            TIME_BEFORE=$(date +%s%N)
+            ./static_rust_linalg --trials=$TRIALS --multiplications=$MULTIPLICATIONS --threads=$THREADS
+            TIME_AFTER=$(date +%s%N)
+            echo "$TRIALS,$MULTIPLICATIONS,$THREADS,$((TIME_AFTER-TIME_BEFORE))"
+        done
+    done
+done
+
+for TRIALS in {1000..10000..1000}
+do
+    for MULTIPLICATIONS in {1000..10000..1000}
+    do
+        for THREADS in 1 2 4 8 16
+        do
+            TIME_BEFORE=$(date +%s%N)
+            ./linalg_profiling --trials=$TRIALS --multiplications=$MULTIPLICATIONS --threads=$THREADS
+            TIME_AFTER=$(date +%s%N)
+            echo "$TRIALS,$MULTIPLICATIONS,$THREADS,$((TIME_AFTER-TIME_BEFORE))"
+        done
+    done
+done
