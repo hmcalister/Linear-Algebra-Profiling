84d3cd0b45f741bc69c136c0771167fa5f45b9d3
Hayden McAlister <haydenmcalister49@gmail.com>
2023-01-29 19:11:10 +1300

    Add dynamic matrix implementation in rust

---
+++ dynamic_rust/src/main.rs
@@ -0,0 +1,78 @@
+use clap::Parser;
+use nalgebra::{DMatrix, DVector};
+use rand::Rng;
+use std::thread;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// The size of the matrix and vectors
+    #[arg(short, long, default_value_t = 100)]
+    n: usize,
+
+    /// Number of trials to attempt
+    #[arg(short, long, default_value_t = 100)]
+    trials: usize,
+
+    /// Number of multiplications per trial
+    #[arg(short, long, default_value_t = 100)]
+    multiplications: usize,
+
+    /// Number of threads to use,
+    /// Should divide number of trials evenly
+    #[arg(long, default_value_t = 1)]
+    threads: usize,
+}
+
+fn heaviside(v: DVector<f64>) -> DVector<f64> {
+    v.map(|i| i.signum())
+}
+
+fn thread_func(_thread_number: &usize, matrix: DMatrix<f64>, num_trials: &usize, steps: &usize) {
+    let mut rng = rand::thread_rng();
+    let n = matrix.nrows();
+
+    for _trial_index in 0..*num_trials {
+        // println!(
+        //     "THREAD {:02} TRIAL: {:05}/{:05}",
+        //     thread_number,
+        //     trial_index + 1,
+        //     num_trials
+        // );
+        let mut v = DVector::<f64>::from_iterator(n, (0..n).map(|_| rng.gen_range(-1.0..1.0)));
+        v = heaviside(v);
+
+        for _step_index in 0..*steps {
+            v = heaviside(&matrix * v);
+        }
+    }
+}
+
+fn main() {
+    let mut rng = rand::thread_rng();
+    let args = Args::parse();
+    let trials_per_thread = args.trials / args.threads;
+    let mut thread_handles = Vec::with_capacity(args.threads);
+    let matrix = DMatrix::<f64>::from_iterator(
+        args.n,
+        args.n,
+        (0..args.n * args.n).map(|_| rng.gen_range(-1.0..1.0)),
+    );
+
+    for thread_index in 0..args.threads {
+        let m_copy = matrix.clone();
+        thread_handles.push(thread::spawn(move || {
+            thread_func(
+                &thread_index,
+                m_copy,
+                &trials_per_thread,
+                &args.multiplications,
+            )
+        }));
+    }
+
+    while !thread_handles.is_empty() {
+        let curr_thread = thread_handles.remove(0);
+        curr_thread.join().unwrap();
+    }
+}
